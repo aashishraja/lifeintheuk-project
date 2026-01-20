47b9e399d447c3c4e29556b44e8cc46f8411bbac
Aashish Raja <aashishraja34@gmail.com>
2026-01-20 23:14:15 +0000

    added quiz logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,30 @@
-import Image from "next/image";
+"use client"
 import prisma from "@/lib/prisma";
-import { revalidatePath } from "next/cache";
-export const dynamic = "force-dynamic"
+import { useRouter } from "next/navigation"
 
 export default function Home() {
+  const router = useRouter()
+
   return (
-    <div> 
-    </div>
-  );
+    <main className="h-screen flex flex-row">
+      <div className="basis-1/2 bg-amber-200 text-amber-950">
+      <button
+  onClick={() => router.push("/resultshistory")}
+  className="underline mt-4"
+>
+  View Results History
+</button>
+
+      </div>
+      <div className="basis-1/2 bg-amber-500 flex flex-col items-center justify-center">
+        Let's Begin
+        <button
+          onClick={() => router.push("/quizpage")}
+          className="px-6 py-3 bg-blue-800 text-white rounded-3xl mt-3"
+        >
+          Start Quiz
+        </button>
+      </div>
+    </main>
+  )
 }
