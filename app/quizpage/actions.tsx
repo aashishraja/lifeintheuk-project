8cc2363f6ff690fe381e3a119267b0f0e3063da0
Aashish Raja <aashishraja34@gmail.com>
2026-01-22 23:18:14 +0000

    added results saving fix-8

--- app/quizpage/actions.tsx
+++ app/quizpage/actions.tsx
@@ -4,6 +4,7 @@ import prisma from "@/lib/prisma"
 
 export async function saveResult(score: number, total: number) {
   console.log("Saving result", score, total)
+  "use server"
   await prisma.quizResult.create({
     data: { score, total },
   })
