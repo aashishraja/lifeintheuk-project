be3c278fb30a3b0604ba8fcd1d28232b03c024ec
Aashish Raja <aashishraja34@gmail.com>
2026-01-23 23:03:44 +0000

    added revalidate path to possibly fix results

--- app/quizpage/actions.tsx
+++ app/quizpage/actions.tsx
@@ -1,6 +1,7 @@
 "use server"
 
 import prisma from "@/lib/prisma"
+import { revalidatePath } from "next/cache";
 
 export async function saveResult(score: number, total: number) {
   console.log("Saving result", score, total)
@@ -8,4 +9,5 @@ export async function saveResult(score: number, total: number) {
   await prisma.quizResult.create({
     data: { score, total },
   })
+  revalidatePath("/resultshistory")
 }
