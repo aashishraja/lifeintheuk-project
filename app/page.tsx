9656a163bdf1c76d18c4f748e1c6439c85e54251
Aashish Raja <aashishraja34@gmail.com>
2026-01-26 01:16:45 +0000

    fixed start page - with flex col

--- app/page.tsx
+++ app/page.tsx
@@ -7,18 +7,19 @@ export default function Home() {
 
   return (
     <main className="h-screen flex md:flex-row flex-col">
-      <div className="basis-1/2 flex flex-col bg-primary">
-        <div className="basis-2/3 p-8 flex md:items-end md:justify-start md:text-5xl font-main text-secondary text-2xl justify-left">
-        Welcome To <br /> Life In The UK <br /> Practice
+      <div className="basis-1/2 flex flex-col bg-primary gap-10 md:items-start md:justify-center items-center md:p-9 px-45">
+        <div className="flex md:items-end md:justify-start md:m-0 md:text-5xl font-main text-secondary text-2xl justify-left mt-8">
+          <span className="block md:text-6xl md:text-left text-5xl text-center md:inline">Welcome To Life In The UK Practice</span>{" "}
         </div>
-        <div className="basis-1/3 p-8">
+        
         <button
         onClick={() => router.push("/resultshistory")}
         className="bg-transparent w-40 p-4 border-2 border-secondary rounded-lg font-secondary text-xl text-secondary hover:bg-secondary hover:border-2 hover:border-secondary hover:text-primary"
         >
         View Results
         </button>
-        </div>
+        
+        
       </div>
       <div className="basis-1/2 bg-tertiary flex flex-col items-center text-5xl justify-center font-main gap-4">
         Let's Begin
